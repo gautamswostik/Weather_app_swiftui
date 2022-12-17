5ad65ff84cb1179a886fc19c94eec41774a0e551
gautamswostik <swastikg11@gmail.com>
2022-12-17 09:04:52 +0545

    Initial Commit

---
+++ WeatherAppSwiftUi/WeatherAppSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUiApp.swift
+//  WeatherAppSwiftUi
+//
+//  Created by swostik gautam on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
